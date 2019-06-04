b5780b60d6acce0efda358497eedca650a96fab6
Jos van Bakel <jos@codeaddict.org>
2019-06-04 11:10:47 +0200

    Update documentation

--- src/feature-detection.ts
+++ src/feature-detection.ts
@@ -1,6 +1,7 @@
 export const mediaDevices = 'mediaDevices' in window.navigator;
 export const setSinkId = 'setSinkId' in (new Audio());
 export const getUserMedia = mediaDevices && 'getUserMedia' in window.navigator.mediaDevices;
+// audiocontext?
 
 const browserUa: string = navigator.userAgent.toLowerCase();
 export const isSafari = (browserUa.indexOf("safari") > -1 && browserUa.indexOf("chrome") < 0);
