c170a6c2f388db3349f76cbdcd7c0dba1f67bce3
Ard Timmerman <ardtimmerman@gmail.com>
2019-06-20 13:54:33 +0200

    Got a working setup where you can recover a lost call even though you are switching between interfaces

--- src/client.ts
+++ src/client.ts
@@ -1,16 +1,14 @@
 import { EventEmitter } from 'events';
 import pRetry from 'p-retry';
-
 import { UA as UABase, Web } from 'sip.js';
+import { ReconnectableTransport } from './reconnectable-transport';
 import { WebCallingSession } from './session';
 import { IWebCallingClientOptions } from './types';
 import { UA } from './ua';
 
-
-interface ISessions {
+export interface ISessions {
   [index: string]: WebCallingSession;
 }
-
 
 export interface IWebCallingClient {
   reconfigure(options: IWebCallingClientOptions): Promise<void>;
@@ -25,127 +23,51 @@ export interface IWebCallingClient {
   on(event: 'subscriptionNotify', listener: (contact: string, state: string) => void): this;
 }
 
-
 export class WebCallingClient extends EventEmitter implements IWebCallingClient {
   public readonly sessions: ISessions = {};
 
-  private ua: UA;
-  private uaOptions: UABase.Options;
-
-  private transportConnectedPromise?: Promise<any>;
-  private unregisteredPromise?: Promise<any>;
-  private registeredPromise?: Promise<any>;
-  private disconnectPromise?: Promise<any>;
-  private registered: boolean = false;
-  private isReconnecting: boolean = false;
-  private retries: number = 10;
+  private transport?: ReconnectableTransport;
 
   constructor(options: IWebCallingClientOptions) {
     super();
 
-    this.configure(options);
+    this.configureTransport(options);
   }
 
   // In the case you want to switch to another account
   public async reconfigure(options: IWebCallingClientOptions) {
     await this.disconnect();
 
-    this.configure(options);
+    this.transport.configure(options);
 
     await this.connect();
   }
 
   // Connect (and subsequently register) to server
   public async connect() {
-    if (!this.ua) {
-      console.log('configuring ua');
-      this.configureUA();
+    if (this.transport.registered) {
+      throw new Error('already connected');
     }
 
-    if (this.unregisteredPromise) {
-      console.error(
-        'Cannot connect while unregistering takes place. Waiting until unregistering is resolved.'
-      );
-
-      await this.unregisteredPromise;
-    }
-
-    if (this.registeredPromise) {
-      return this.registeredPromise;
-    }
-
-    this.registeredPromise = new Promise((resolve, reject) => {
-      this.ua.once('registered', () => {
-        this.registered = true;
-        resolve(true);
-      });
-      this.ua.once('registrationFailed', e => reject(e));
-    });
-
-    this.ua.start();
-
-    try {
-      await this.transportConnectedPromise;
-    } catch (e) {
-      throw new Error(`Tried to connect ${this.retries + 1} times, didn't work. Sorry.`);
-    }
-
-    this.ua.register();
-
-    return this.registeredPromise;
+    await this.transport.connect();
   }
 
   // Unregister (and subsequently disconnect) to server
-  public async disconnect(): Promise<void> {
-    if (this.disconnectPromise) {
-      return this.disconnectPromise;
-    }
+  public async disconnect({ unregister = true } = {}): Promise<void> {
+    await this.transport.disconnect({ unregister });
 
-    if (!this.ua) {
-      throw new Error('not connected');
-    }
-
-    delete this.registeredPromise;
-
-    if (this.registered) {
-      this.unregisteredPromise = new Promise(resolve =>
-        this.ua.once('unregistered', () => {
-          this.registered = false;
-          resolve(true);
-        })
-      );
-
-      console.log('trying to unregister..');
-      this.ua.unregister();
-
-      // Little protection to make sure our account is actually unregistered
-      // and received an ACK before other functions are called
-      // (i.e. ua.disconnect)
-      await this.unregisteredPromise;
-    }
-
-    console.log('unregistered!');
-
-    await this.ua.disconnect();
-
-    console.log('disconnected!');
-
-    this.ua.transport.removeAllListeners();
-    this.ua.removeAllListeners();
-
-    delete this.ua;
-    delete this.unregisteredPromise;
+    this.transport.close();
   }
 
   public async invite(phoneNumber: string) {
-    if (!this.registeredPromise) {
+    if (!this.transport || !this.transport.registeredPromise) {
       throw new Error('Register first!');
     }
 
-    await this.registeredPromise;
+    await this.transport.registeredPromise;
 
     const session = new WebCallingSession({
-      session: this.ua.invite(phoneNumber)
+      session: this.transport.invite(phoneNumber)
     });
 
     this.sessions[session.id] = session;
@@ -156,76 +78,25 @@ export class WebCallingClient extends EventEmitter implements IWebCallingClient 
   }
 
   public async subscribe(contact: string): Promise<void> {
-    return Promise.reject("not implemented");
+    return Promise.reject('not implemented');
   }
 
   public async unsubscribe(contact: string): Promise<void> {
-    return Promise.reject("not implemented");
+    return Promise.reject('not implemented');
   }
 
-  private async reconnect(): Promise<boolean> {
-    this.isReconnecting = true;
+  private configureTransport(options) {
+    this.transport = new ReconnectableTransport(options);
 
-    try {
-      await pRetry(this.connect.bind(this), {
-        maxTimeout: 30000,
-        minTimeout: 500,
-        onFailedAttempt: error => {
-          console.log(
-            `Connection attempt ${error.attemptNumber} failed. There are ${
-              error.retriesLeft
-            } retries left.`
-          );
-        },
-        randomize: true,
-        retries: this.retries
-      });
-      return true;
-    } catch (e) {
-      console.log('Not attempting to reconnect anymore');
-      return false;
-    }
-  }
-
-  private configureUA() {
-    this.ua = new UA(this.uaOptions);
-
-    this.transportConnectedPromise = new Promise((resolve, reject) => {
-      this.ua.once('transportCreated', () => {
-        console.log('transport created');
-        this.ua.transport.once('connected', () => {
-          console.log('connected');
-          resolve();
-        });
-
-        this.ua.transport.once('disconnected', async e => {
-          console.log('unexpected disconnect');
-
-          await this.disconnect();
-
-          if (this.isReconnecting) {
-            // Rejecting here to make sure that the reconnect promise in
-            // pRetry is catched and can be properly retried.
-            reject(e);
-
-            // Returning here to make sure that reconnect is not called again.
-            return;
-          }
-
-          const connected = await this.reconnect();
-
-          this.isReconnecting = false;
-
-          if (connected) {
-            resolve();
-          } else {
-            reject(e);
-          }
-        });
+    this.transport.on('reviveSessions', () => {
+      Object.values(this.sessions).forEach(session => {
+        console.log(session);
+        session.rebuildSessionDescriptionHandler();
+        session.reinvite();
       });
     });
 
-    this.ua.on('invite', uaSession => {
+    this.transport.on('invite', uaSession => {
       const session = new WebCallingSession({
         session: uaSession
       });
@@ -236,50 +107,5 @@ export class WebCallingClient extends EventEmitter implements IWebCallingClient 
 
       session.once('terminated', () => delete this.sessions[session.id]);
     });
-  }
-
-  private get defaultOptions() {
-    return {
-      autostart: false,
-      autostop: false,
-      log: {
-        builtinEnabled: true,
-        connector: undefined,
-        level: 'warn'
-      },
-      noAnswerTimeout: 60,
-      register: false,
-      registerOptions: {
-        expires: 3600
-      },
-      userAgentString: 'vialer-calling-lib'
-    };
-  }
-
-  private configure(options: IWebCallingClientOptions) {
-    const { account, transport } = options;
-
-    this.uaOptions = {
-      ...this.defaultOptions,
-      authorizationUser: account.user,
-      password: account.password,
-      sessionDescriptionHandlerFactoryOptions: {
-        constraints: { audio: true, video: false },
-        modifiers: [
-          Web.Modifiers.stripVideo,
-        ],
-        peerConnectionOptions: {
-          rtcConfiguration: {
-            iceServers: transport.iceServers.map((s: string) => ({ urls: s }))
-          }
-        }
-      },
-      transportOptions: {
-        maxReconnectionAttempts: 0,
-        traceSip: true,
-        wsServers: transport.wsServers
-      },
-      uri: account.uri
-    };
   }
 }
--- src/ua.ts
+++ src/ua.ts
@@ -1,13 +1,108 @@
-import { RegisterContext, UA as UABase, UAStatus } from 'sip.js';
+import pTimeout from 'p-timeout';
+import {
+  C as SIPConstants,
+  Core,
+  InviteClientContext,
+  InviteServerContext,
+  RegisterContext,
+  SessionDescriptionHandlerModifiers,
+  SessionStatus,
+  UA as UABase,
+  UAStatus,
+  URI,
+  Web
+} from 'sip.js';
 
+export class WrappedInviteClientContext extends InviteClientContext {
+  /**
+   * Reconfigure the WebRTC peerconnection.
+   */
+  public rebuildSessionDescriptionHandler() {
+    console.log('session descrioption handler is rebuild!!');
+    this.sessionDescriptionHandler = this.sessionDescriptionHandlerFactory(
+      this,
+      this.ua.configuration.sessionDescriptionHandlerFactoryOptions || {}
+    );
+  }
+}
+
+// tslint:disable-next-line: max-classes-per-file
+export class WrappedInviteServerContext extends InviteServerContext {
+  /**
+   * Reconfigure the WebRTC peerconnection.
+   */
+  public rebuildSessionDescriptionHandler() {
+    console.log('session descrioption handler is rebuild!!');
+    this.sessionDescriptionHandler = this.sessionDescriptionHandlerFactory(
+      this,
+      this.ua.configuration.sessionDescriptionHandlerFactoryOptions || {}
+    );
+  }
+}
+
+// tslint:disable-next-line: max-classes-per-file
+export class WrappedTransport extends Web.Transport {
+  /**
+   * Disconnect socket. It could happen that the user switches network
+   * interfaces while calling. If this happens, closing a websocket will
+   * cause it to be blocked. To make sure that UA gets to the proper internal
+   * state so that it is ready to 'switch over' to the new network interface
+   * with a new websocket, we call the function that normally causes the
+   * disconnectPromise to be resolved after a timeout.
+   */
+  protected disconnectPromise(options: any = {}): Promise<any> {
+    return pTimeout(super.disconnectPromise(), 1000, () => {
+      console.log('calling on close by myself!');
+      (this as any).onClose({ code: 'fake', reason: 'Artificial timeout' });
+    }).then(() => ({ overrideEvent: true }));
+  }
+}
+
+// tslint:disable-next-line: max-classes-per-file
 export class UA extends UABase {
   private disconnectPromise: Promise<void>;
+
+  constructor(configuration: UABase.Options) {
+    super(configuration);
+
+    this.configureUADelegate();
+  }
 
   public disconnect(): Promise<void> {
     this.stop();
     return this.disconnectPromise;
   }
 
+  /**
+   * Make an outgoing call.
+   *
+   * @param {String} target
+   * @param {Object} views
+   * @param {Object} [options.media] gets passed to SIP.sessionDescriptionHandler.getDescription as mediaHint
+   *
+   * @throws {TypeError}
+   *
+   */
+  public invite(
+    target: string | URI,
+    options?: InviteClientContext.Options,
+    modifiers?: SessionDescriptionHandlerModifiers
+  ): WrappedInviteClientContext {
+    const context: WrappedInviteClientContext = new WrappedInviteClientContext(
+      this,
+      target,
+      options,
+      modifiers
+    );
+    // Delay sending actual invite until the next 'tick' if we are already
+    // connected, so that API consumers can register to events fired by the
+    // the session.
+    this.transport.afterConnected(() => {
+      context.invite();
+      this.emit('inviteSent', context);
+    });
+    return context;
+  }
   /**
    * Gracefully close.
    */
@@ -75,5 +170,81 @@ export class UA extends UABase {
     }
 
     return this;
+  }
+
+  /*
+   * Below is taken from the UABase constructor, modified a little to enable
+   * returning a extended InviteServerContext possible.
+   */
+  private configureUADelegate() {
+    // The Replaces header contains information used to match an existing
+    // SIP dialog (call-id, to-tag, and from-tag).  Upon receiving an INVITE
+    // with a Replaces header, the User Agent (UA) attempts to match this
+    // information with a confirmed or early dialog.
+    // https://tools.ietf.org/html/rfc3891#section-3
+    const handleInviteWithReplacesHeader = (
+      context: InviteServerContext,
+      request: Core.IncomingRequestMessage
+    ): void => {
+      if (this.configuration.replaces !== SIPConstants.supported.UNSUPPORTED) {
+        const replaces = request.parseHeader('replaces');
+        if (replaces) {
+          const targetSession =
+            this.sessions[replaces.call_id + replaces.replaces_from_tag] ||
+            this.sessions[replaces.call_id + replaces.replaces_to_tag] ||
+            undefined;
+          if (!targetSession) {
+            this.userAgentCore.replyStateless(request, { statusCode: 481 });
+            return;
+          }
+          if (targetSession.status === SessionStatus.STATUS_TERMINATED) {
+            this.userAgentCore.replyStateless(request, { statusCode: 603 });
+            return;
+          }
+          const targetDialogId =
+            replaces.call_id + replaces.replaces_to_tag + replaces.replaces_from_tag;
+          const targetDialog = this.userAgentCore.dialogs.get(targetDialogId);
+          if (!targetDialog) {
+            this.userAgentCore.replyStateless(request, { statusCode: 481 });
+            return;
+          }
+          if (!targetDialog.early && replaces.early_only) {
+            this.userAgentCore.replyStateless(request, { statusCode: 486 });
+            return;
+          }
+          context.replacee = targetSession;
+        }
+      }
+    };
+
+    this.userAgentCore.delegate.onInvite = (
+      incomingInviteRequest: Core.IncomingInviteRequest
+    ): void => {
+      // FIXME: Ported - 100 Trying send should be configurable.
+      // Only required if TU will not respond in 200ms.
+      // https://tools.ietf.org/html/rfc3261#section-17.2.1
+      incomingInviteRequest.trying();
+      incomingInviteRequest.delegate = {
+        onCancel: (cancel: Core.IncomingRequestMessage): void => {
+          context.onCancel(cancel);
+        },
+        onTransportError: (error: Core.TransportError): void => {
+          context.onTransportError();
+        }
+      };
+
+      // begin change
+      const context = new WrappedInviteServerContext(this, incomingInviteRequest);
+      // end change
+
+      // Ported - handling of out of dialog INVITE with Replaces.
+      handleInviteWithReplacesHeader(context, incomingInviteRequest.message);
+      // Ported - make the first call to progress automatically.
+      if (context.autoSendAnInitialProvisionalResponse) {
+        context.progress();
+      }
+      console.log('incoming request being delegated.');
+      this.emit('invite', context);
+    };
   }
 }
