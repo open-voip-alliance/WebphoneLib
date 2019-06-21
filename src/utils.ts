50fee98c225ee76da40977901ca569b297dd76f9
Ard Timmerman <ardtimmerman@gmail.com>
2019-06-21 14:56:10 +0200

    Fixed some MR discussions

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,3 @@
-import { SubscriptionStatus } from './enums';
-
 export function eqSet<T>(a: Set<T>, b: Set<T>): boolean {
   return a.size === b.size && [...a].every(b.has.bind(b));
 }
@@ -28,41 +26,4 @@ export function jitter(interval, percentage) {
   const min = 0 - Math.ceil(interval * (percentage / 100));
   const max = Math.floor(interval * (percentage / 100));
   return Math.floor(Math.random() * (max - min)) + min;
-}
-
-/**
- * Parse an incoming dialog XML request body and return
- * the account state from it.
- * @param {Request} notification - A SIP.js Request object.
- * @returns {String} - The state of the account.
- */
-export function statusFromDialog(notification) {
-  const parser = new DOMParser();
-  const xmlDoc = parser ? parser.parseFromString(notification.request.body, 'text/xml') : null;
-  const dialogNode = xmlDoc ? xmlDoc.getElementsByTagName('dialog-info')[0] : null;
-  // Skip; an invalid dialog.
-  if (!dialogNode) {
-    return null;
-  }
-
-  const stateNode = dialogNode.getElementsByTagName('state')[0];
-  let state = SubscriptionStatus.AVAILABLE;
-
-  // State node has final say, regardless of stateAttr!
-  if (stateNode) {
-    switch (stateNode.textContent) {
-      case SubscriptionStatus.TRYING:
-      case SubscriptionStatus.PROCEEDING:
-      case SubscriptionStatus.EARLY:
-        state = SubscriptionStatus.RINGING;
-        break;
-      case SubscriptionStatus.CONFIRMED:
-        state = SubscriptionStatus.BUSY;
-        break;
-      case SubscriptionStatus.TERMINATED:
-        state = SubscriptionStatus.AVAILABLE;
-        break;
-    }
-  }
-  return state;
 }
