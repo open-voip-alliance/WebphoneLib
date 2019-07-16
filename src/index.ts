4974ba6262c638021fe3757d682f2ef123d87498
Ard Timmerman <ardtimmerman@gmail.com>
2019-07-16 09:55:24 +0200

    Added ClientStatus to index

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { Autoplay } from './autoplay';
 import { Client } from './client';
+import { ClientStatus } from './enums';
 import * as Features from './features';
 import { log } from './logger';
 import { Media } from './media';
 import { Sound } from './sound';
 
-export { Autoplay, Client, Features, log, Media, Sound };
+export { Autoplay, Client, ClientStatus, Features, log, Media, Sound };
