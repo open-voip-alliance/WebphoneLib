16927de0238248270c511fc6b51a0c13feee04cd
Ard Timmerman <ardtimmerman@gmail.com>
2019-09-19 11:52:54 +0200

    These are not the globals you are looking for

--- src/audio-context.ts
+++ src/audio-context.ts
@@ -1,5 +1,5 @@
 function createAudioContext(): AudioContext {
-  if ('window' in global) {
+  if ('window' in window) {
     const cls = (window as any).AudioContext || (window as any).webkitAudioContext;
     if (cls) {
       return new cls();
--- src/features.ts
+++ src/features.ts
@@ -2,7 +2,7 @@ const mediaDevices = 'mediaDevices' in window.navigator;
 
 export const webaudio = {
   mediaDevices,
-  setSinkId: 'Audio' in global && 'setSinkId' in new Audio(),
+  setSinkId: 'Audio' in window && 'setSinkId' in new Audio(),
   getUserMedia: mediaDevices && 'getUserMedia' in window.navigator.mediaDevices,
   audioContext: 'AudioContext' in window || 'webkitAudioContext' in window
 };
