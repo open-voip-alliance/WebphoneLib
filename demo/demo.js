9b98de2d11f3dfaa89139b2fd40291a1c7d50b25
Ard Timmerman <ardtimmerman@gmail.com>
2019-12-02 11:23:37 +0100

    Removed some vulnerabilities

--- demo/demo.js
+++ demo/demo.js
@@ -32,7 +32,7 @@ const account = {
 };
 
 const transport = {
-  wsServers: `wss://websocket-staging.${CONF.yourPlatformURL}`,
+  wsServers: `wss://websocket.${CONF.yourPlatformURL}`,
   iceServers: []
 };
 
