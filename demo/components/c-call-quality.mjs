72b97204928e0f807618755d98f098e992cafa73
Ard Timmerman <ardtimmerman@gmail.com>
2020-07-15 16:37:07 +0200

    Removed unused function

--- demo/components/c-call-quality.mjs
+++ demo/components/c-call-quality.mjs
@@ -3,20 +3,6 @@ import * as sipClient from '../lib/calling.mjs';
 import { empty } from '../utils/dom.mjs';
 import { NodesProxy } from '../utils/elementProxies.mjs';
 
-function createTableRow(data) {
-  const { config, mos } = data;
-  const last = (mos.last || 0).toFixed(2);
-  const low = (mos.lowest || 0).toFixed(2);
-  const high = (mos.highest || 0).toFixed(2);
-  const avg = (mos.average || 0).toFixed(2);
-
-  this.nodes.config.innerText = config;
-  this.nodes.lowest.innerText = `Low: ${low}`;
-  this.nodes.highest.innerText = `High: ${high}`;
-  this.nodes.average.innerText = `Average:  ${avg}`;
-  this.nodes.last.innerText = `Last:  ${last}`;
-}
-
 window.customElements.define(
   'c-call-quality',
   class extends HTMLElement {
