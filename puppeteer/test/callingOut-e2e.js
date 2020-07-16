87c1b287dd29bc6785cae09b90dd9c3019e1ea61
dagmar <dagmar.hoogendorp@wearespindle.com>
2020-07-16 17:06:31 +0200

    so far...

--- puppeteer/test/callingOut-e2e.js
+++ puppeteer/test/callingOut-e2e.js
@@ -1,22 +1,34 @@
 const puppeteer = require('puppeteer');
 const expect = require('chai').expect;
 const { click, typeText, clearText } = require('../lib/helpers');
-const { USER_A, USER_B, PASSWORD_A, PASSWORD_B } = require('../config.js');
+const { USER_A, USER_B, PASSWORD_A, PASSWORD_B, NUMBER_A, NUMBER_B } = require('../config.js');
+const { UrlMapping } = require('typedoc');
 
 const USER_ID_INPUT = 'c-voip-account [data-selector="userIdInput"]';
 const USER_PASSWORD_INPUT = 'c-voip-account [data-selector="passwordInput"]';
 
+const DIALER_INPUT = 'c-dialer [data-selector="input"]';
+const DIALER_CALL_BUTTON = 'c-dialer [data-action="call"]';
+
+const REGISTER_BUTTON = 'c-voip-account [data-action="register"]';
+
 describe('examples', () => {
   let browser;
   let page;
+  let context;
 
   beforeEach(async function() {
     browser = await puppeteer.launch({
+      args: ['--use-fake-device-for-media-stream'],
       headless: false,
       slowMo: 10,
       devtools: false
     });
-    page = await browser.newPage();
+    const url = new URL('localhost:1235/demo/');
+    context = browser.defaultBrowserContext();
+    context.overridePermissions(url.origin, ['microphone']);
+    page = await context.newPage();
+    page.on('error', msg => console.log('PAGE LOG:', msg));
     await page.setDefaultTimeout(1000);
   });
 
@@ -27,6 +39,11 @@ describe('examples', () => {
   it('Should launch a browser', async function() {
     // Assert if the page is visible
     await page.goto('localhost:1235/demo/');
+
+    const granted = await page.evaluate(async () => {
+      return (await navigator.permissions.query({ name: 'microphone' })).state;
+    });
+    console.log('Granted:', granted);
     // expect(page).to.include('c-voip-account')
     const url = await page.url();
     expect(url).to.include('/demo/');
@@ -38,13 +55,14 @@ describe('examples', () => {
     await typeText(page, USER_PASSWORD_INPUT, PASSWORD_A);
 
     // Click on Register
-    await click(page, 'c-voip-account [data-action="register"]');
+    await click(page, REGISTER_BUTTON);
     await page.waitFor(2000);
-    await page.screenshot({ path: 'screenshots/page1.png' });
+    await page.screenshot({ path: 'screenshots/page1.png', fullPage: true });
 
-    const page2 = await browser.newPage();
+    const page2 = await context.newPage();
+    page2.on('console', msg => console.log('PAGE LOG:', msg.text));
 
-    await page2.setDefaultTimeout(1000);
+    await page2.setDefaultTimeout(500);
     await page2.goto('localhost:1235/demo/');
 
     await clearText(page2, USER_ID_INPUT);
@@ -54,11 +72,18 @@ describe('examples', () => {
     await typeText(page2, USER_PASSWORD_INPUT, PASSWORD_B);
 
     // Click on Register
-    await click(page2, 'c-voip-account [data-action="register"]');
-    await page2.waitFor(2000);
-    await page2.screenshot({ path: 'screenshots/page2.png' });
+    await click(page2, REGISTER_BUTTON);
+    await page2.waitFor(500);
+    await page2.screenshot({ path: 'screenshots/page2.png', fullPage: true });
 
-    // Assert if both pages are visible
-    await browser.close();
+    await clearText(page2, DIALER_INPUT);
+    await typeText(page2, DIALER_INPUT, NUMBER_A);
+
+    await click(page2, DIALER_CALL_BUTTON);
+    await page2.waitFor(500);
+    await page2.screenshot({ path: 'screenshots/page_call_2.png', fullPage: true });
+    await page.waitFor(2000);
+
+    await page.screenshot({ path: 'screenshots/page_call.png', fullPage: true });
   });
 });
