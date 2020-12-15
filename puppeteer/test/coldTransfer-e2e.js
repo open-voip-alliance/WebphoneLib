f00d6e3b9d295f486cb5fbc7044c62033f69e3a8
dagmar <dagmar.hoogendorp@wearespindle.com>
2020-12-15 17:01:03 +0100

    three tests

--- puppeteer/test/coldTransfer-e2e.js
+++ puppeteer/test/coldTransfer-e2e.js
@@ -16,18 +16,15 @@ const {
   PASSWORD_A,
   PASSWORD_B,
   PASSWORD_C,
-  NUMBER_A,
   NUMBER_B,
   NUMBER_C
 } = require('../config');
 const {
   NON_EXISTING_NUMBER,
   DEMO_URL,
-  REGISTER_BUTTON,
   SESSIONS_LIST,
   SESSION_ACCEPT_BUTTON,
   SESSION_REJECT_BUTTON,
-  SESSION_CANCEL_BUTTON,
   SESSION_HANGUP_BUTTON,
   SESSION_STATUS,
   SESSION_TRANSFER_BUTTON,
@@ -104,9 +101,22 @@ describe('Cold Transfer', () => {
     page2.bringToFront();
     await click(page2, SESSION_ACCEPT_BUTTON);
     expect(await waitForText(page2, SESSION_STATUS, 'active')).to.be.true;
+    await click(page2, SESSION_HANGUP_BUTTON);
   });
 
-  xit('Have user A call user B and transfer user C to user B', async function() {
+  it('Have user A call user B and transfer user C to user B but have user C hang up and let user A accept ringback', async function() {
+    page2.bringToFront();
+    await page2.goto(DEMO_URL);
+
+    await registerUser(page2, USER_B, PASSWORD_B);
+    expect(await waitForText(page2, CLIENT_STATUS, 'connected')).to.be.true;
+
+    page3.bringToFront();
+    await page3.goto(DEMO_URL);
+
+    await registerUser(page3, USER_C, PASSWORD_C);
+    expect(await waitForText(page3, CLIENT_STATUS, 'connected')).to.be.true;
+
     page.bringToFront();
     await page.goto(DEMO_URL);
 
@@ -114,25 +124,41 @@ describe('Cold Transfer', () => {
     expect(url).to.include('/demo/');
 
     await registerUser(page, USER_A, PASSWORD_A);
-    await click(page, REGISTER_BUTTON);
     expect(await waitForText(page, CLIENT_STATUS, 'connected')).to.be.true;
 
+    await callNumber(page, NUMBER_B);
+
+    page2.bringToFront();
+    await click(page2, SESSION_ACCEPT_BUTTON);
+    expect(await waitForText(page2, SESSION_STATUS, 'active')).to.be.true;
+
+    page.bringToFront();
+    await click(page, SESSION_TRANSFER_BUTTON);
+
+    await page.select(SESSION_TRANSFER_METHOD_DROPDOWN, SESSION_COLD_TRANSFER_SELECT);
+    await typeText(page, SESSION_TRANSFER_INPUT, NUMBER_C);
+    await click(page, SESSION_COMPLETE_TRANSFER_BUTTON);
+    expect(await waitForSelector(page, SESSIONS_LIST)).to.be.empty;
+
+    page3.bringToFront();
+    // Rejecting the incoming transfer call
+    await click(page3, SESSION_REJECT_BUTTON);
+    expect(await waitForSelector(page, SESSIONS_LIST)).to.be.empty;
+
+    // Go back to user A to accept the ringback
+    page.bringToFront();
+    await click(page, SESSION_ACCEPT_BUTTON);
+    expect(await waitForText(page, SESSION_STATUS, 'active')).to.be.true;
+    await click(page, SESSION_HANGUP_BUTTON);
+  });
+
+  it('Have user A call user B and transfer a non existing number to user B', async function() {
     page2.bringToFront();
     await page2.goto(DEMO_URL);
 
     await registerUser(page2, USER_B, PASSWORD_B);
-    await click(page2, REGISTER_BUTTON);
     expect(await waitForText(page2, CLIENT_STATUS, 'connected')).to.be.true;
 
-    page3.bringToFront();
-    await page3.goto(DEMO_URL);
-
-    await registerUser(page3, USER_C, PASSWORD_C);
-    await click(page3, REGISTER_BUTTON);
-    expect(await waitForText(page3, CLIENT_STATUS, 'connected')).to.be.true;
-  });
-
-  xit('Have user A call user B and transfer user C to user B but have user C hang up and let user A accept ringback', async function() {
     page.bringToFront();
     await page.goto(DEMO_URL);
 
@@ -140,47 +166,29 @@ describe('Cold Transfer', () => {
     expect(url).to.include('/demo/');
 
     await registerUser(page, USER_A, PASSWORD_A);
-    await click(page, REGISTER_BUTTON);
     expect(await waitForText(page, CLIENT_STATUS, 'connected')).to.be.true;
 
+    await callNumber(page, NUMBER_B);
+
     page2.bringToFront();
-    await page2.goto(DEMO_URL);
+    await click(page2, SESSION_ACCEPT_BUTTON);
+    expect(await waitForText(page2, SESSION_STATUS, 'active')).to.be.true;
 
-    await registerUser(page2, USER_B, PASSWORD_B);
-    await click(page2, REGISTER_BUTTON);
-    expect(await waitForText(page2, CLIENT_STATUS, 'connected')).to.be.true;
-
-    page3.bringToFront();
-    await page3.goto(DEMO_URL);
-
-    await registerUser(page3, USER_C, PASSWORD_C);
-    await click(page3, REGISTER_BUTTON);
-    expect(await waitForText(page3, CLIENT_STATUS, 'connected')).to.be.true;
-  });
-
-  xit('Have user A call user B and transfer user C to user B', async function() {
     page.bringToFront();
-    await page.goto(DEMO_URL);
+    await click(page, SESSION_TRANSFER_BUTTON);
 
-    const url = await page.url();
-    expect(url).to.include('/demo/');
-
-    await registerUser(page, USER_A, PASSWORD_A);
-    await click(page, REGISTER_BUTTON);
-    expect(await waitForText(page, CLIENT_STATUS, 'connected')).to.be.true;
+    await page.select(SESSION_TRANSFER_METHOD_DROPDOWN, SESSION_COLD_TRANSFER_SELECT);
+    await typeText(page, SESSION_TRANSFER_INPUT, NON_EXISTING_NUMBER);
+    await click(page, SESSION_COMPLETE_TRANSFER_BUTTON);
+    expect(await waitForSelector(page, SESSIONS_LIST)).to.be.empty;
 
     page2.bringToFront();
-    await page2.goto(DEMO_URL);
+    expect(await waitForText(page2, SESSION_STATUS, 'active')).to.be.true;
 
-    await registerUser(page2, USER_B, PASSWORD_B);
-    await click(page2, REGISTER_BUTTON);
-    expect(await waitForText(page2, CLIENT_STATUS, 'connected')).to.be.true;
-
-    page3.bringToFront();
-    await page3.goto(DEMO_URL);
-
-    await registerUser(page3, USER_C, PASSWORD_C);
-    await click(page3, REGISTER_BUTTON);
-    expect(await waitForText(page3, CLIENT_STATUS, 'connected')).to.be.true;
+    //Accept the ringback
+    page.bringToFront();
+    await click(page, SESSION_ACCEPT_BUTTON);
+    expect(await waitForText(page, SESSION_STATUS, 'active')).to.be.true;
+    await click(page, SESSION_HANGUP_BUTTON);
   });
 });
