901f2ccdc6c5e10120a09badccacf7822a7d83e4
dagmar <dagmar.hoogendorp@wearespindle.com>
2020-12-16 10:45:59 +0100

    small fix

--- puppeteer/test/callingOut-e2e.js
+++ puppeteer/test/callingOut-e2e.js
@@ -44,7 +44,7 @@ describe('Calling out', () => {
 
   it('calling out & having the other party answer & let the other party end the call (terminate)', async function() {
     page.bringToFront();
-    await page.goto('http://localhost:1235/demo');
+    await page.goto(DEMO_URL);
 
     const url = await page.url();
     expect(url).to.include('/demo/');
