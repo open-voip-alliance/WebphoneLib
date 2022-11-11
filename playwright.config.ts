c263331290e8c881b1f60006d69785ea506a12ef
Alena Silchanka <alena.silchanka@wearespindle.com>
2022-11-11 10:52:09 +0100

    renamed tests folder to e2e

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ dotenv.config();
  * See https://playwright.dev/docs/test-configuration.
  */
 const config: PlaywrightTestConfig = {
-  testDir: './tests',
+  testDir: './e2e',
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
   expect: {
