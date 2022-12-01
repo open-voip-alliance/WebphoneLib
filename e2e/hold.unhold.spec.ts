c0e12be88b8ca79a0433abb6808b59e79c42d3af
Alena Silchanka <alena.silchanka@wearespindle.com>
2022-12-01 16:30:22 +0100

    added hold/unhold spec

---
+++ e2e/hold.unhold.spec.ts
@@ -0,0 +1,42 @@
+import { test } from '@playwright/test';
+
+import { HelpFunctions } from './helpers/helpers';
+
+test.describe('Hold/unhold', () => {
+  let helpersA: HelpFunctions;
+  let helpersB: HelpFunctions;
+
+  test.beforeEach(async ({ context }) => {
+    await context.grantPermissions(['microphone']);
+    const pageUserA = await context.newPage();
+    const pageUserB = await context.newPage();
+    helpersA = new HelpFunctions(pageUserA);
+    helpersB = new HelpFunctions(pageUserB);
+
+    await pageUserA.goto(`${process.env.DEMO_URL}`);
+    await helpersA.registerUser(`${process.env.USER_A}`, `${process.env.PASSWORD_A}`);
+    await helpersA.assertClientConnected();
+
+    await pageUserB.goto(`${process.env.DEMO_URL}`);
+    await helpersB.registerUser(`${process.env.USER_B}`, `${process.env.PASSWORD_B}`);
+    await helpersB.assertClientConnected();
+  });
+
+  test.only('calling out & the other party answers & put the call on hold/unhold', async () => {
+    await helpersA.callNumber(`${process.env.NUMBER_B}`);
+    await helpersA.assertSessionStatus('ringing');
+    await helpersB.assertSessionExists();
+
+    await helpersB.acceptCall();
+    await helpersA.assertSessionStatus('active');
+    await helpersB.assertSessionStatus('active');
+
+    await helpersB.holdCall();
+    await helpersA.assertSessionStatus('active');
+    await helpersB.assertSessionStatus('on_hold');
+
+    await helpersB.unholdCall();
+    await helpersA.assertSessionStatus('active');
+    await helpersB.assertSessionStatus('active');
+  });
+});
