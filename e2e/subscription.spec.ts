a6e65e03f1ecb9fa0fff52caf6967209d9fec62f
Alena Silchanka <alena.silchanka@wearespindle.com>
2022-12-02 21:40:07 +0100

    Added test for ringing status

--- e2e/subscription.spec.ts
+++ e2e/subscription.spec.ts
@@ -5,13 +5,16 @@ import { HelpFunctions } from './helpers/helpers';
 test.describe('Hold/unhold', () => {
   let helpersA: HelpFunctions;
   let helpersB: HelpFunctions;
+  let helpersC: HelpFunctions;
 
   test.beforeEach(async ({ context }) => {
     await context.grantPermissions(['microphone']);
     const pageUserA = await context.newPage();
     const pageUserB = await context.newPage();
+    const pageUserC = await context.newPage();
     helpersA = new HelpFunctions(pageUserA);
     helpersB = new HelpFunctions(pageUserB);
+    helpersC = new HelpFunctions(pageUserC);
 
     await pageUserA.goto(`${process.env.DEMO_URL}`);
     await helpersA.registerUser(`${process.env.USER_A}`, `${process.env.PASSWORD_A}`);
@@ -20,10 +23,21 @@ test.describe('Hold/unhold', () => {
     await pageUserB.goto(`${process.env.DEMO_URL}`);
     await helpersB.registerUser(`${process.env.USER_B}`, `${process.env.PASSWORD_B}`);
     await helpersB.assertAccountStatus('connected');
+
+    await pageUserC.goto(`${process.env.DEMO_URL}`);
+    await helpersC.registerUser(`${process.env.USER_C}`, `${process.env.PASSWORD_C}`);
+    await helpersC.assertAccountStatus('connected');
   });
 
-  test.only('User sees the status of the contact he subscribed to', async () => {
+  test('User sibscribed to another contact and sees their status', async () => {
     await helpersA.subsribeToContact(`${process.env.USER_B}`);
     await helpersA.assertSubscribedContactStatus(`${process.env.USER_B}`, 'available');
+  });
+
+  test('After the subscribed contact gets an incoming call its status changes to ringing', async () => {
+    await helpersA.subsribeToContact(`${process.env.USER_B}`);
+
+    await helpersC.callNumber(`${process.env.NUMBER_B}`);
+    await helpersA.assertSubscribedContactStatus(`${process.env.USER_B}`, 'ringing');
   });
 });
