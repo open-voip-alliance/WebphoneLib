ea17cabdba7dc7b6e373ea3e5b77423f34bbecff
dagmar <dagmar.hoogendorp@wearespindle.com>
2026-01-20 11:35:46 +0100

    fix: more updates

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
 import builtins from 'rollup-plugin-node-builtins';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import json from 'rollup-plugin-json';
-import typescript from 'rollup-plugin-typescript2';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import typescript from '@rollup/plugin-typescript';
 
 function onwarn(warning) {
   console.log(warning.toString());
@@ -21,6 +21,13 @@ export default {
     commonjs(),
     builtins(),
     json(),
-    typescript({ tsconfig: './tsconfig.build.json' })
+    typescript({
+      tsconfig: './tsconfig.build.json',
+      compilerOptions: {
+        outDir: 'dist',
+        declaration: true,
+        declarationDir: 'dist'
+      }
+    })
   ]
 };
--- src/health-checker.ts
+++ src/health-checker.ts
@@ -3,7 +3,7 @@ import { Core } from 'sip.js';
 import { UserAgent } from 'sip.js/lib/api/user-agent';
 
 export class HealthChecker {
-  private optionsTimeout: NodeJS.Timeout;
+  private optionsTimeout: ReturnType<typeof setTimeout>;
   private logger: Core.Logger;
 
   constructor(private userAgent: UserAgent) {
